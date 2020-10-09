ce01eecb0a8c9fc8dacec12be80011fef057497a
Sean Crowley <crowley.p.sean@gmail.com>
2020-10-09 07:17:41 +0100

    Write data out to InfluxDB

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # MotherNatureManager
+
+- https://learn.adafruit.com/adafruit-ccs811-air-quality-sensor/raspberry-pi-wiring-test
+- https://github.com/adafruit/Adafruit_CircuitPython_CCS811
+- https://tutorials-raspberrypi.com/raspberry-pi-measure-humidity-temperature-dht11-dht22/
--- main.py
+++ main.py
@@ -1,26 +1,51 @@
+from datetime import datetime
+from influxdb import InfluxDBClient
+
 import time
 import board
 import busio
 import adafruit_ccs811
 import Adafruit_DHT
 
-DHT_SENSOR = Adafruit_DHT.DHT11
+# Sensor Setup
+I2C = busio.I2C(board.SCL, board.SDA)
 DHT_PIN = 4
-i2c = busio.I2C(board.SCL, board.SDA)
-ccs811 = adafruit_ccs811.CCS811(i2c)
+DHT_SENSOR = Adafruit_DHT.DHT11
+CCS811_SENSOR = adafruit_ccs811.CCS811(I2C)
+
+# Database Setup
+client = InfluxDBClient(host='localhost', port=8086)
+client.switch_database('environments')
 
 # Wait for the co2 sensor to be ready
-while not ccs811.data_ready:
+while not CCS811_SENSOR.data_ready:
     pass
 
 while True:
     try:
         humidity, tempC = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
         tempF = (tempC * (9/5)) + 32
-        co2 = ccs811.eco2
-        tvoc = ccs811.tvoc
+        co2 = CCS811_SENSOR.eco2
+        tvoc = CCS811_SENSOR.tvoc
 
         print("TEMP: {0:0.1f} F / {1:0.1f} C, HUMIDITY: {2:0.1f} %, CO2: {3} PPM, TVOC: {4} PPB".format(tempF, tempC, humidity, co2, tvoc))
+
+        json = [{
+            "measurement": "environment",
+            "tags": {
+                "environment": "env1"
+            },
+            "time": datetime.now(),
+            "fields": {
+                "tempF": tempF,
+                "tempC": tempC,
+                "humidity": humidity,
+                "co2": co2,
+                "tvoc": tvoc
+            }
+        }]
+
+        client.write_points(json)
 
         time.sleep(1);
 
