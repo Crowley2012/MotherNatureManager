3cf496bd709875b72c5ce6b82a36c7c793c86010
Sean Crowley <crowley.p.sean@gmail.com>
2020-10-16 22:21:01 +0100

    fix date format

--- main.py
+++ main.py
@@ -28,14 +28,14 @@ while True:
         co2 = CCS811_SENSOR.eco2
         tvoc = CCS811_SENSOR.tvoc
 
-        print("TEMP: {0:0.1f} F / {1:0.1f} C, HUMIDITY: {2:0.1f} %, CO2: {3} PPM, TVOC: {4} PPB".format(tempF, tempC, humidity, co2, tvoc))
+        #print("TEMP: {0:0.1f} F / {1:0.1f} C, HUMIDITY: {2:0.1f} %, CO2: {3} PPM, TVOC: {4} PPB".format(tempF, tempC, humidity, co2, tvoc))
 
         json = [{
             "measurement": "environment",
             "tags": {
                 "environment": "env1"
             },
-            "time": datetime.now(),
+            "time": datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ'),
             "fields": {
                 "tempF": tempF,
                 "tempC": tempC,
